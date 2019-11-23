0e06ec146382383eef47138a11b255a50a514a6c
Valeria Karon <vkaron@at-f5.21-school.ru>
2019-11-23 18:41:35 +0300

    add fix project

---
+++ src/clear.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   clear.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vkaron <vkaron@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/21 15:16:15 by vkaron            #+#    #+#             */
+/*   Updated: 2019/11/23 18:14:20 by vkaron           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rtv1.h"
+
+int		free_words(char **words, int res)
+{
+	int i;
+
+	i = 0;
+	while (words[i])
+		free(words[i++]);
+	free(words);
+	return (res);
+}
+
+void	free_figs(t_fig *figs)
+{
+	t_fig *cur;
+	t_fig *prev;
+
+	if (!figs)
+		return ;
+	prev = figs;
+	while (prev->next)
+	{
+		cur = prev->next;
+		free(prev);
+		prev = cur;
+	}
+	free(prev);
+}
+
+void	free_lghts(t_lght *lghts)
+{
+	t_lght *cur;
+	t_lght *prev;
+
+	if (!lghts)
+		return ;
+	prev = lghts;
+	while (prev->next)
+	{
+		cur = prev->next;
+		free(prev);
+		prev = cur;
+	}
+	free(prev);
+}
+
+void	free_l(t_lst *lst)
+{
+	if (lst->set)
+		free(lst->set);
+	if (lst->scn)
+	{
+		if (lst->scn->figs)
+			free_figs(lst->scn->figs);
+		if (lst->scn->lghts)
+			free_lghts(lst->scn->lghts);
+		free(lst->scn);
+	}
+	if (lst->img)
+		mlx_destroy_image(lst->mlx, lst->img);
+	if (lst->win)
+		mlx_destroy_window(lst->mlx, lst->win);
+	if (lst->mlx)
+		free(lst->mlx);
+	free(lst);
+}
+
+int		free_word_line(char **line, char **word)
+{
+	int i;
+
+	i = 0;
+	if (word)
+	{
+		while (word[i])
+		{
+			free(word[i]);
+			word[i] = NULL;
+			++i;
+		}
+		free(word);
+		word = NULL;
+	}
+	if (*line)
+	{
+		free(*line);
+		*line = NULL;
+	}
+	return (i);
+}
